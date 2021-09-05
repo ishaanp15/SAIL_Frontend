25f0b3a2a94313a687346222528d263ee941576a
ishaanp15 <ishaanp1502@gmail.com>
2021-09-05 18:08:45 +0530

    ishaan1

--- src/app/allComponents/login/login.component.ts
+++ src/app/allComponents/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, OnInit,Output } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -7,14 +8,40 @@ import { Component, EventEmitter, OnInit,Output } from '@angular/core';
 })
 
 export class LoginComponent implements OnInit {
-
+  url = 'https://localhost:3000/login'; 
+  postData = {username:'username',
+      password:'password'};
   @Output("ngToggle") ngToggle: EventEmitter<any> = new EventEmitter();
-  constructor() { }
+  constructor(public http: HttpClient) { }
 
   ngOnInit(): void {
+    
   }
+
+ 
+  
+
+  OnInputUser(event: any) {
+    this.postData.username = event.target.value;
+    console.log(this.postData.username);
+    }
+
+  OnInputPass(event: any) {
+    this.postData.password = event.target.value;
+    }
+
+   loginClick(): void{
+
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json'});
+    this.http.post<any>(this.url, this.postData,{headers})
+    .toPromise().then(data =>
+      { console.log(data);
+      });
+
+   }
+
   onClick(): void {
-    this.ngToggle.emit();
+  this.ngToggle.emit();
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClient, HttpHeaders, HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -16,7 +16,8 @@ import { LoginComponent } from './allComponents/login/login.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
